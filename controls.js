59f78e49db91f9dc6cac0b2950797c12bebdad27
6121280030@nation.ac.th <6121280030@nation.ac.th>
2023-07-29 13:32:02 +0700

    Committed

--- controls.js
+++ controls.js
@@ -4,8 +4,9 @@ const navbarItems = document.querySelectorAll(".navbar-link");
 navbarItems.forEach( nav => {
     nav.addEventListener('click', function(e) {
         if ( this.classList.contains("active") ) return !1;
-        for ( let index = 0; index < navbarItems; index++ ) {
-            navbarItems[ index ] !== this ? navbarItems[ index ].classList.remove("active") : this.classList.add("active");
+        console.log( new Date().toLocaleString() + " actived")
+        for ( let index = 0; index < navbarItems.length; index++ ) {
+            navbarItems[ index ] !== this ? navbarItems[ index ].classList.remove("active") : navbarItems[ index ].classList.add("active");
         }
     })
 })
